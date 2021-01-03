8f6bcfbc491b3d0b8233f9b4d4bf6339bebbfdff
hutao <2320702073@qq.com>
2021-01-04 01:53:17 +0800

    annivesary 新增详细item的删除与修改后保存功能

--- pages/anniversary/detail/detail.js
+++ pages/anniversary/detail/detail.js
@@ -22,11 +22,61 @@ Page({
       detailId: op.id
     })
   },
+  inputEvent: function(e){
+    //传value
+    this.setData({
+      event: e.detail.value
+    })
+  },
   saveEvent: function() {
-
+    var id = this.data.detailId
+    var pages=getCurrentPages()
+    var date=this.data.date
+    var aDate = date.split("-")
+    var oDate = new Date(aDate[1] + "-" + aDate[2] + "-" + aDate[0])
+    var curDate = new Date()
+    var days = parseInt(Math.abs(curDate - oDate) / (1000 * 60 * 60 * 24)) 
+    var anniver = { 'text': this.data.event, 'date': this.data.date, 'time': days, 'id': this.data.detailId}
+    wx.navigateBack({
+      success:function(res){
+        var Anniver = pages[0].data.Anniversary
+        for(var i = 0; i < Anniver.length; i++){
+          if(Anniver[i].id === id){
+            Anniver[i]=anniver
+          }
+        }
+        pages[0].setData({
+          Anniversary:Anniver
+        })
+        wx.setStorage({
+          key: 'Anniversary',
+          data: Anniver,
+        })        
+      }
+    })
   },
   delEvent: function(){
-
+    var id = this.data.detailId
+    var pages = getCurrentPages()
+    wx.navigateBack({
+      success: function(res){
+        var Anniver = pages[0].data.Anniversary
+        for(var i = 0; i < Anniver.length; i++){
+          if(Anniver[i].id === id){
+            Anniver.splice(i, 1)
+            break
+          }
+        }
+        pages[0].setData({
+          Anniversary: Anniver,
+          shiwTip: Anniver.length === 0 ? true : false
+        })
+        wx.setStorage({
+          key: 'Anniversary',
+          data: Anniver,
+        })        
+      }
+    })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
